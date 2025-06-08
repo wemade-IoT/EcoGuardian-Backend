0fea3774034f462a1382d3bcb468c6f7f5f58a7f
GustavoHC <u202213983@upc.edu.pe>
2025-06-08 15:54:46 -0500

    feat(planing): add planning bc infrastructure layer

---
+++ EcoGuardian-Backend/Planning/Infrastructure/Persistence/EFC/Repositories/OrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using EcoGuardian_Backend.Planning.Domain.Model.Aggregates;
+using EcoGuardian_Backend.Planning.Domain.Repositories;
+using EcoGuardian_Backend.Shared.Infrastructure.Persistence.EFC.Configuration;
+using EcoGuardian_Backend.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoGuardian_Backend.Planning.Infrastructure.Persistence.EFC.Repositories;
+
+public class OrderRepository(AppDbContext context) : BaseRepository<Order>(context), IOrderRepository
+{
+    public async Task<IEnumerable<Order>> GetOrdersByUserIdAsync(int userId)
+    {
+        return await context.Set<Order>()
+            .Where(x => x.UserId == userId)
+            .ToListAsync();
+    }
+}
