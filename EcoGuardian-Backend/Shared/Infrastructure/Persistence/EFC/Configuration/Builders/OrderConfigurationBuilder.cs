d296d86cb830f3b2cab328863864eeff8513cfa7
GustavoHC <u202213983@upc.edu.pe>
2025-06-08 15:50:20 -0500

    feat(planning): add configuration builders for planning bc

---
+++ EcoGuardian-Backend/Shared/Infrastructure/Persistence/EFC/Configuration/Builders/OrderConfigurationBuilder.cs
@@ -0,0 +1,27 @@
+﻿using EcoGuardian_Backend.Planning.Domain.Model.Aggregates;
+using EcoGuardian_Backend.Planning.Domain.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcoGuardian_Backend.Shared.Infrastructure.Persistence.EFC.Configuration.Builders;
+
+public class OrderConfigurationBuilder : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+        builder.Property(x => x.Action)
+            .IsRequired();
+        builder.Property(x => x.UserId)
+            .IsRequired();
+        builder.Property(x => x.SensorId)
+            .IsRequired();
+        builder.Property(x => x.ActuatorId)
+            .IsRequired();
+        builder.Property(x => x.CreatedAt);
+        builder.Property(x => x.CompletedAt);
+        builder.HasOne<OrderState>().WithMany().HasForeignKey(x => x.StateId);
+    }
+}
