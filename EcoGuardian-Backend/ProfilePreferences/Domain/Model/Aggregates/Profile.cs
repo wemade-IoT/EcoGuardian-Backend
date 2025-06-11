b35c7540addec658b061f39a4cbf8d50257b1471
U202210138 <u202210138@upc.edu.pe>
2025-06-11 08:22:35 -0500

    feat: Added Profile Aggregate

---
+++ EcoGuardian-Backend/ProfilePreferences/Domain/Model/Aggregates/Profile.cs
@@ -0,0 +1,33 @@
+using EcoGuardian_Backend.ProfilePreferences.Domain.Model.Commands;
+
+namespace EcoGuardian_Backend.ProfilePreferences.Domain.Model.Aggregates;
+
+public class Profile
+{
+    public int Id { get; }
+    
+    public string Name { get; private set; }
+    
+    public string UserName { get; private set; }
+    
+    public string Email { get; private set; }
+    
+    public string Address { get; private set; }
+    
+    public string AvatarUrl { get; private set; }
+    
+    public int UserId { get; private set; }
+    
+    public int SubscriptionId { get; private set; }
+
+    public Profile()
+    {
+        Name = string.Empty;
+        UserName = string.Empty;
+        Email = string.Empty;
+        Address = string.Empty;
+        UserId = 0;
+        SubscriptionId = 0;
+        AvatarUrl = string.Empty;
+    }
+
