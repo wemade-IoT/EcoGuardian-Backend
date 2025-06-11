d9fdace62b6ac4e6481ec519bbd0d1795aa7b21d
U202210138 <u202210138@upc.edu.pe>
2025-06-11 08:41:41 -0500

    feat: added ProfileResourceFromEntityAssembler class

---
+++ EcoGuardian-Backend/ProfilePreferences/Interfaces/REST/Trasform/ProfileResourceFromEntityAssembler.cs
@@ -0,0 +1,21 @@
+using EcoGuardian_Backend.ProfilePreferences.Domain.Model.Aggregates;
+using EcoGuardian_Backend.ProfilePreferences.Interfaces.REST.Resources;
+
+namespace EcoGuardian_Backend.ProfilePreferences.Interfaces.REST.Trasform;
+
+public class ProfileResourceFromEntityAssembler
+{
+    public static ProfileResource ToResourceFromEntity(Profile entity)
+    {
+        return new ProfileResource(
+            entity.Id,
+            entity.Name,
+            entity.UserName,
+            entity.Email,
+            entity.Address,
+            entity.AvatarUrl,
+            entity.UserId,
+            entity.SubscriptionId
+        );
+    }
+}
