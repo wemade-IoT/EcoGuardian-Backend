156ef6ff500e8472279a6e2ce1c79d2533ee156f
Manuel Peña <u202210138@upc.edu.pe>
2025-07-05 19:25:25 -0500

    feat: Added integration tests for PlantController
    Introduces PlantControllerIntegrationTests covering GetPlantsByUserId, GetPlantById, and CreatePlant endpoints. Tests verify correct HTTP responses and expected behaviors using mocked services.

---
+++ EcoGuardian-UnitTests/PlantControllerIntegrationTest.cs
@@ -0,0 +1,67 @@
+using EcoGuardian_Backend.OperationAndMonitoring.Domain.Model.Aggregates;
+using EcoGuardian_Backend.OperationAndMonitoring.Domain.Model.Commands;
+using EcoGuardian_Backend.OperationAndMonitoring.Domain.Model.Queries;
+using EcoGuardian_Backend.OperationAndMonitoring.Domain.Services;
+using EcoGuardian_Backend.OperationAndMonitoring.Interfaces.REST;
+using EcoGuardian_Backend.OperationAndMonitoring.Interfaces.REST.Resources;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EcoGuardian_UnitTests
+{
+    [TestFixture]
+    public class PlantControllerIntegrationTests
+    {
+        private Mock<IPlantCommandService> _mockCommandService;
+        private Mock<IPlantQueryService> _mockQueryService;
+        private PlantController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockCommandService = new Mock<IPlantCommandService>();
+            _mockQueryService = new Mock<IPlantQueryService>();
+            _controller = new PlantController(_mockCommandService.Object, _mockQueryService.Object);
+        }
+
+        [Test]
+        public async Task GetPlantsByUserId_ReturnsOk()
+        {
+            _mockQueryService.Setup(s => s.Handle(It.IsAny<GetPlantsByUserIdQuery>())).ReturnsAsync(new List<Plant>());
+            var result = await _controller.GetPlantsByUserId(1) as OkObjectResult;
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.StatusCode, Is.EqualTo(200));
+            Assert.That(result.Value, Is.InstanceOf<List<PlantResource>>());
+        }
+
+        [Test]
+        public async Task GetPlantById_NotFound()
+        {
+            _mockQueryService.Setup(s => s.Handle(It.IsAny<GetPlantByIdQuery>())).ReturnsAsync((Plant?)null);
+            var result = await _controller.GetPlantById(99999) as NotFoundResult;
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.StatusCode, Is.EqualTo(404));
+        }
+
+        [Test]
+        public async Task CreatePlant_ReturnsCreated()
+        {
+            var mockFile = new Mock<IFormFile>().Object;
+            var resource = new CreatePlantResource(
+                "IntegrationTestPlant", mockFile, "TestType", 10, 1, 0.5, 0.5, 0.5, false, 1
+            );
+            _mockCommandService.Setup(s => s.Handle(It.IsAny<CreatePlantCommand>())).ReturnsAsync(new Plant());
+            var result = await _controller.CreatePlant(resource) as ObjectResult;
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.StatusCode, Is.EqualTo(201));
+            Assert.That(result.Value, Is.Not.Null);
+            var value = result.Value;
+            var messageProp = value.GetType().GetProperty("message");
+            var idProp = value.GetType().GetProperty("id");
+            Assert.That(messageProp, Is.Not.Null);
+            Assert.That(idProp, Is.Not.Null);
+            Assert.That(messageProp.GetValue(value, null), Is.EqualTo("Planta creada exitosamente"));
+        }
+    }
+}
